c3618c19db5002f70dcda09478210a4a72b1df84
Jose Antonio Esquivel <jesquivel@aeropost.com>
2016-09-17 18:29:40 -0600

    v1

---
+++ src/main.cpp
@@ -0,0 +1,86 @@
+// Buttons Panel
+
+// Let the IDE point to the Souliss framework
+#include <SoulissFramework.h>
+
+// Configure the framework
+#include "bconf/StandardArduino.h"				// Use a standard Arduino
+#include "conf/usart.h"							// USART RS485
+
+/*************/
+// Use the following if you are using an RS485 transceiver with
+// transmission enable pin, otherwise delete this section.
+//
+#define USARTDRIVER_INSKETCH
+#define USART_TXENABLE			1
+#define USART_TXENPIN			3
+#define USARTDRIVER				Serial
+/*************/
+
+// Include framework code and libraries
+#include <SPI.h>
+#include <Souliss.h>
+
+#include "../../ControllerGateway/include/constants.h"
+
+// Pin assignment
+#define BUTTON_1_PIN		4
+#define BUTTON_2_PIN		5
+#define LED_1_PIN			6
+#define LED_2_PIN			7
+
+void setup()
+{
+	Initialize();
+
+	// Set network parameters
+	SetAddress(RS485_BUTTONS_PANEL_1_ADDR, VNET_SUBNET_MASK, RS485_GATEWAY_ADDR);
+
+	// Define inputs, outputs pins and pulldown
+	pinMode(BUTTON_1_PIN, INPUT);
+	pinMode(BUTTON_2_PIN, INPUT);
+	pinMode(LED_1_PIN, OUTPUT);
+	pinMode(LED_2_PIN, OUTPUT);
+
+	Set_T11(BUTTONS_PANEL_1_BUTTON_1_SLOT);
+	Set_T11(BUTTONS_PANEL_1_BUTTON_2_SLOT);
+	Set_T11(BUTTONS_PANEL_1_LED_1_SLOT);
+	Set_T11(BUTTONS_PANEL_1_LED_2_SLOT);
+}
+
+void loop()
+{
+	// Here we start to play
+	EXECUTEFAST() {
+		UPDATEFAST();
+
+		FAST_50ms() {	// Process  logic and relevant input and output every 50 milliseconds
+
+			if(DigIn(BUTTON_1_PIN, Souliss_T1n_ToggleCmd, BUTTONS_PANEL_1_BUTTON_1_SLOT))
+				Send(RS485_GATEWAY_ADDR, GATEWAY_BUTTONS_PANEL_1_BUTTON_1_SLOT, Souliss_T1n_ToggleCmd);
+
+			if(DigIn(BUTTON_2_PIN, Souliss_T1n_ToggleCmd, BUTTONS_PANEL_1_BUTTON_2_SLOT))
+				Send(RS485_GATEWAY_ADDR, GATEWAY_BUTTONS_PANEL_1_BUTTON_2_SLOT, Souliss_T1n_ToggleCmd);
+
+			// Execute the logic
+			Logic_T11(BUTTONS_PANEL_1_BUTTON_1_SLOT);
+			Logic_T11(BUTTONS_PANEL_1_BUTTON_2_SLOT);
+			Logic_T11(BUTTONS_PANEL_1_LED_1_SLOT);
+			Logic_T11(BUTTONS_PANEL_1_LED_2_SLOT);
+
+			// Drive the LEDs
+			DigOut(LED_1_PIN, Souliss_T1n_Coil, BUTTONS_PANEL_1_LED_1_SLOT);
+			DigOut(LED_2_PIN, Souliss_T1n_Coil, BUTTONS_PANEL_1_LED_2_SLOT);
+		}
+
+		// Process the communication, this include the command that are coming from SoulissApp
+		// or from the push-button located on the other node
+		FAST_PeerComms();
+
+	}
+
+	EXECUTESLOW() {
+		UPDATESLOW();
+	}
+
+}
